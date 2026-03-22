5cab25992b36e9c0b3d93f06167ffb13ecef6307
WassCodeur <wachioubouraima56@gmail.com>
2026-03-22 22:53:51 +0000

    feat: add routes for speakers, schedule, venue, registration, contact, about, teams, coc, health security, sponsors, cfp, and volunteers for 2026; add db.sqlite3 and *.sql to .gitignore; create 2026_utils.py

--- app/routers/router_2026.py
+++ app/routers/router_2026.py
@@ -27,3 +27,135 @@ def home(request: Request):
             "year": year,
         },
     )
+
+
+@router.get("/speakers")
+def speakers(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_speakers.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/schedule")
+def schedule(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_schedule.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/venue")
+def venue(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_venue.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/registration")
+def registration(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_registration.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/contact")
+def contact(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_contact.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/about")
+def about(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_about.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/teams")
+def teams(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_teams.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/coc")
+def coc(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_coc.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/health_security")
+def health_security(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_health_security.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/sponsors")
+def sponsors(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_sponsors.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/cfp")
+def cfp(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_cfp.html",
+        context={
+            "year": year,
+        },
+    )
+
+
+@router.get("/volunteers")
+def volunteers(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="2026_volunteers.html",
+        context={
+            "year": year,
+        },
+    )
