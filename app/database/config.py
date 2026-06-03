6e21d52fb4899a5c24908ff3ffeb30e32a299f79
Steventog <yematog@gmail.com>
2026-06-03 23:15:58 +0000

    feat: add job board page

--- app/database/config.py
+++ app/database/config.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("SUPABASE_URL", "")
+key: str = os.environ.get("SUPABASE_KEY", "")
 
-supabase: Client = create_client(url, key)
+supabase: Client | None = create_client(url, key) if url and key else None
