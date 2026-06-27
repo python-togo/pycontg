01f4e821e9302eab8866cac7cbc1c861e0365e4d
WassCodeur <wachioubouraima56@gmail.com>
2026-06-27 16:48:41 +0000

    feat: update redirect URL for road to PyCon

--- app/routers/router_2026.py
+++ app/routers/router_2026.py
@@ -1527,7 +1527,7 @@ def _30days(request: Request):
 
 @router.get("/road-to-pycon")
 def _road_to_pycon(request: Request):
-    return RedirectResponse(url="https://www.youtube.com/live/IEnL6fCmmXY?si=Em9QBUfL1fsKVmp4", status_code=302)
+    return RedirectResponse(url="https://www.youtube.com/live/AHlmLFM5_ps?si=ofCdk192DeriY6OS", status_code=302)
 
 
 @router.get("/streamyard")
